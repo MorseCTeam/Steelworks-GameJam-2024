e9f53feaee18e4d1a39ad28dd49862ec0f0a7b10
liitlelenim <jakubblaszczyk20@gmail.com>
2024-02-23 23:36:19 +0100

    Created basic robot controller

--- Assets/Scripts/RobotController.cs
+++ Assets/Scripts/RobotController.cs
@@ -1,18 +1,63 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class RobotController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Direction CurrentDirection { get; private set; } = Direction.Up;
+    public bool IsBusy { get; private set; } = false;
+
+    [SerializeField] private float oneTileMovementLength = 0.25f;
+
+    public void Move(int amountOfTiles)
     {
-   
+        if(IsBusy)
+        {
+            Debug.LogWarning("Tried to use robot method while robot was busy.");
+            return;
+        }
+
+        IsBusy = true;
+        transform
+            .DOMove(transform.position + (Vector3)DirectionToVector(CurrentDirection) * amountOfTiles,
+                oneTileMovementLength * amountOfTiles)
+            .OnComplete(() => { IsBusy = false; });
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Rotate(Direction direction)
     {
+        if(IsBusy)
+        {
+            Debug.LogWarning("Tried to use robot method while robot was busy.");
+            return;
+        }
         
+        IsBusy = true;
+        
+        CurrentDirection = direction;
+        
+        IsBusy = false;
     }
+    
+    public Vector2 DirectionToVector(Direction direction)
+    {
+        return direction switch
+        {
+            Direction.Up => Vector2.up,
+            Direction.Down => Vector2.down,
+            Direction.Left => Vector2.left,
+            Direction.Right => Vector2.right,
+            _ => throw new ArgumentException("Illegal direction? How is that possible?")
+        };
+    }
+}
+
+public enum Direction
+{
+    Up,
+    Down,
+    Left,
+    Right
 }
