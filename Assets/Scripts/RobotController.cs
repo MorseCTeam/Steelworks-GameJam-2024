84e42bb5f6d69b831ebdf0fcbcedca823f6f6d25
liitlelenim <jakubblaszczyk20@gmail.com>
2024-02-24 00:36:21 +0100

    upgraded robot controller for better collision

--- Assets/Scripts/RobotController.cs
+++ Assets/Scripts/RobotController.cs
@@ -1,7 +1,5 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
-using DG.Tweening;
 using UnityEngine;
 
 public class RobotController : MonoBehaviour
@@ -10,38 +8,71 @@ public class RobotController : MonoBehaviour
     public bool IsBusy { get; private set; } = false;
 
     [SerializeField] private float oneTileMovementLength = 0.25f;
-    
+
+    private Rigidbody2D _rigidbody;
+
+    private int testingTilesAmount;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        //Testing purposes only
+        if (Input.GetKeyDown(KeyCode.P)) testingTilesAmount++;
+        if (Input.GetKeyDown(KeyCode.Space)) Move(testingTilesAmount);
+        if (Input.GetKeyDown(KeyCode.DownArrow)) Rotate(Direction.Down);
+        if (Input.GetKeyDown(KeyCode.UpArrow)) Rotate(Direction.Up);
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) Rotate(Direction.Left);
+        if (Input.GetKeyDown(KeyCode.RightArrow)) Rotate(Direction.Right);
+    }
+
     public void Move(int amountOfTiles)
     {
-        if(IsBusy)
+        if (IsBusy)
         {
             Debug.LogWarning("Tried to use robot method while robot was busy.");
             return;
         }
 
         IsBusy = true;
-        transform
-            .DOMove(transform.position + (Vector3)DirectionToVector(CurrentDirection) * amountOfTiles,
-                oneTileMovementLength * amountOfTiles)
-            .OnComplete(() => { IsBusy = false; })
-            .SetEase(Ease.Linear);
+        SnapBackToGrid();
+        StartCoroutine(MovementCoroutine(amountOfTiles));
+    }
+
+    public IEnumerator MovementCoroutine(int amountOfTiles)
+    {
+        _rigidbody.velocity = DirectionToVector(CurrentDirection) / oneTileMovementLength;
+        
+        yield return new WaitForSeconds(amountOfTiles * oneTileMovementLength);
+        _rigidbody.velocity = Vector2.zero;
+        SnapBackToGrid();
+        IsBusy = false;
     }
 
     public void Rotate(Direction direction)
     {
-        if(IsBusy)
+        if (IsBusy)
         {
             Debug.LogWarning("Tried to use robot method while robot was busy.");
             return;
         }
-        
+
         IsBusy = true;
-        
+
         CurrentDirection = direction;
-        
+
         IsBusy = false;
     }
-    
+
+    private void SnapBackToGrid()
+    {
+        transform.position = new Vector3(Mathf.RoundToInt(transform.position.x),
+            Mathf.RoundToInt(transform.position.y),
+            Mathf.RoundToInt(transform.position.z));
+    }
     public Vector2 DirectionToVector(Direction direction)
     {
         return direction switch
