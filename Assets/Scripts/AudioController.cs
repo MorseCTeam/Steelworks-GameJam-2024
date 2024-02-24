0ca1307539c6262882b8e9705658e80a616b3c7a
BestIsaacPlayer <milosz.plask@gmail.com>
2024-02-24 16:27:17 +0100

    added audio controller

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class AudioController : MonoBehaviour
+{
+    [SerializeField] private List<Sound> sounds;
+
+    AudioSource Play(SoundType type, float volume = 1, bool loop = false)
+    {
+        var foundSound = sounds.FirstOrDefault(sound => sound.Type == type);
+        if (foundSound.Clip == null || foundSound.Clip.Length == 0)
+        {
+            throw new ArgumentException("Could not find audioclip with given type");
+        }
+
+        AudioClip foundClip = foundSound.Clip[Random.Range(0, foundSound.Clip.Length)];
+
+        GameObject createdObject = new GameObject("Audio Source");
+        AudioSource createdSource = createdObject.AddComponent<AudioSource>();
+
+        createdSource.clip = foundClip;
+        createdSource.volume = volume;
+        createdSource.pitch = Random.Range(0.95f, 1.05f);
+        createdSource.loop = loop;
+        
+        return createdSource;
+    }
+}
+
+[Serializable]
+public class Sound
+{
+   [field: SerializeField] public SoundType Type { get; set; }
+   [field: SerializeField] public AudioClip[] Clip { get; set; }
+}
+
+public enum SoundType
+{
+    ComputerBuzz
+}
