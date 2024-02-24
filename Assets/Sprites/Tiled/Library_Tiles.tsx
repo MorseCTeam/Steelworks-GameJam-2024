a21aa03820b562ad5bc9400f5432fa1cce56d39b
BestIsaacPlayer <milosz.plask@gmail.com>
2024-02-24 22:21:17 +0100

    kasztan mówi nie, szefo mówi tak

--- Assets/Sprites/Tiled/Library_Tiles.tsx
+++ Assets/Sprites/Tiled/Library_Tiles.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Library_Tiles" tilewidth="16" tileheight="32" tilecount="3" columns="0">
+<tileset version="1.10" tiledversion="1.10.2" name="Library_Tiles" tilewidth="48" tileheight="48" tilecount="5" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="16" height="16" source="image.png"/>
@@ -11,6 +11,18 @@
   <image width="16" height="32" source="sciana.png"/>
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="18.2267" width="16" height="13.7733"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <image width="16" height="48" source="poka_bok.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="-0.03125" width="16" height="48.0313"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <image width="48" height="24" source="poka_front.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="48" height="24"/>
   </objectgroup>
  </tile>
 </tileset>
