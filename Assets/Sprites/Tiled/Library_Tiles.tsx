f231d62a8274b039b144820ff6d2146ac6c9da83
BestIsaacPlayer <milosz.plask@gmail.com>
2024-02-24 19:52:10 +0100

    dupson

---
+++ Assets/Sprites/Tiled/Library_Tiles.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Library_Tiles" tilewidth="16" tileheight="32" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="16" height="16" source="image.png"/>
+ </tile>
+ <tile id="1">
+  <image width="16" height="16" source="podloga2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="16" height="32" source="sciana.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="18.2267" width="16" height="13.7733"/>
+  </objectgroup>
+ </tile>
+</tileset>
